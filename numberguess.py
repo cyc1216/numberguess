6481e800169813281273351f9691750d6e6fcb0c
YC Chen <chency1216@gmail.com>
2018-12-26 15:32:29 +0800

    加入猜的次數

--- numberguess.py
+++ numberguess.py
@@ -4,12 +4,16 @@
 # 猜錯的話,告訴使用者 比答案大/小
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 #count = count + 1 的快寫
 	a = int(input('請輸入答案:'))
 	if a == r:
-		print("你猜對了")
+		print("你猜對了") 
+		print('你已經猜', count, '次')
 		break
 	elif a > r:
 		print("比答案大")
 	else:
 		print("比答案小")
+	print('你已經猜', count, '次')
